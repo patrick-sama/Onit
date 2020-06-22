c7e1b462d5987d72f1a8a94c0264ce8331eb1b0a
patrick noumsi <wafonoumsi@gmail.com>
2020-06-22 13:37:12 +0200

    git init

---
+++ Onit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Onit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
