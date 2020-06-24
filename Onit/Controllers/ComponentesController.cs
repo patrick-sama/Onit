bb975909f22966f5c9fd21b7757fa0044db38bf2
patrick noumsi <wafonoumsi@gmail.com>
2020-06-25 00:22:59 +0200

    Prima domanda fatta , problema nel javascript da corregere

--- Onit/Controllers/ComponentesController.cs
+++ Onit/Controllers/ComponentesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Onit.Data;
 using Onit.Models;
+using Onit.Other;
 
 namespace Onit.Controllers
 {
@@ -25,60 +27,23 @@ namespace Onit.Controllers
             return View(await _context.Componente.ToListAsync());
         }
 
-        // GET: Componentes/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: Componentes/AddOrEdit/5
+        // GET: Componentes/AddOrEdit/
+        public async Task<IActionResult> AddOrEdit(int id = 0)
         {
-            if (id == null)
+            if (id == 0)
             {
-                return NotFound();
+                return View(new Componente());
             }
-
-            var componente = await _context.Componente
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (componente == null)
+            else
             {
-                return NotFound();
+                var componente = await _context.Componente.FindAsync(id);
+                if (componente == null)
+                {
+                    return NotFound();
+                }
+                return View(componente);
             }
-
-            return View(componente);
-        }
-
-        // GET: Componentes/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Componentes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Codice,Descrizione,Note")] Componente componente)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(componente);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(componente);
-        }
-
-        // GET: Componentes/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var componente = await _context.Componente.FindAsync(id);
-            if (componente == null)
-            {
-                return NotFound();
-            }
-            return View(componente);
         }
 
         // POST: Componentes/Edit/5
@@ -86,34 +51,37 @@ namespace Onit.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Codice,Descrizione,Note")] Componente componente)
+        public async Task<IActionResult> AddOrEdit(int id, [Bind("Id,Codice,Descrizione,Note")] Componente componente)
         {
-            if (id != componente.Id)
-            {
-                return NotFound();
-            }
-
             if (ModelState.IsValid)
             {
-                try
+                if (id == 0)
                 {
-                    _context.Update(componente);
+                    _context.Add(componente);
                     await _context.SaveChangesAsync();
                 }
-                catch (DbUpdateConcurrencyException)
+                else
                 {
-                    if (!ComponenteExists(componente.Id))
+                    try
                     {
-                        return NotFound();
+                        _context.Update(componente);
+                        await _context.SaveChangesAsync();
                     }
-                    else
+                    catch (DbUpdateConcurrencyException)
                     {
-                        throw;
+                        if (!ComponenteExists(componente.Id))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
                     }
+                    return Json(new { isValid = true, html = Helper.RenderRazorViewToString(this, "_ViewAll", _context.Componente.ToList()) });
                 }
-                return RedirectToAction(nameof(Index));
             }
-            return View(componente);
+            return Json(new { isValid = false, html = Helper.RenderRazorViewToString(this, "AddOrEdit", componente) });
         }
 
         // GET: Componentes/Delete/5
--- Onit/wwwroot/js/site.js
+++ Onit/wwwroot/js/site.js
@@ -1,19 +1,43 @@
-﻿var Locazioni = [];
-function LoadLocazione(element) {
-    if (Locazioni.length == 0) {
-        $.ajax({
-            type: "GET",
-            url: '/home/getLocazioneArea',
-            success: function (data) {
-
-            }
-        })
-    }
+﻿showInPopup = (url, title) => {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function (res) {
+            $('#form-modal .modal-body').html(res);
+            $('#form-modal .modal-title').html(title);
+            $('#form-modal').modal('show');
+        }
+    })
 }
 
-
-function Prova() {
-    $(function () {
-        $("#createComponent").modal();
-    });
+ajaxPost = form => {
+    try {
+        $.ajax({
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.isValid) {
+                    $('#view-all').html(res.html)
+                    $('#form-modal .modal-body').html('');
+                    $('#form-modal .modal-title').html('');
+                    $('#form-modal').modal('hide');
+                }
+                else {
+                    $('#form-modal .modal-body').html(res.html);
+                    //$('#form-modal').modal('hide');
+                }
+                 
+            },
+            error: function (err) {
+                console.log(err)
+            }
+        })
+        //to prevent default form submit event
+        return false;
+    } catch (ex) {
+        console.log(ex)
+    }
 }
